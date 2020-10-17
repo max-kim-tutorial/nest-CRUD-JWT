a36296a0beb7b139d13ff5511c0903b4c10b5395
Max Kim <hwaseen@gmail.com>
2020-10-17 19:37:12 +0900

    feat:env setting and create postgres DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from '@nestjs/config';
+import User from "./models/user.entity";
+import Post from "./models/post.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User, Post],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
