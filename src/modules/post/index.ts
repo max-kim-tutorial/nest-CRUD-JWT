d059e7b713fa5862dc5c4ede52ebbccdcf78b15d
Max Kim <hwaseen@gmail.com>
2020-10-17 21:29:40 +0900

    chore:add comment

--- src/modules/post/index.ts
+++ src/modules/post/index.ts
@@ -5,7 +5,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import Post from 'src/models/post.entity';
 
 @Module({
-  // 데이터베이스 모듈 import했다고 선언해주기
+  // @InjectRepository 사용했다고 모듈에게 알려주기
+  // repository도 있으니 connection이나 entityManager도 데코레이터로 사용할 수 있겠네
+  // 이런식으로 표시해주면 알아서 dependency를 해결한다
   imports: [TypeOrmModule.forFeature([Post])],
   providers: [PostService],
   controllers: [PostController]
